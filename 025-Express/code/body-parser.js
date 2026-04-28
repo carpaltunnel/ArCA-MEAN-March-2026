afe3e42fa6ebeabe6c93241da9b3d9da4c0c6d8e
carpaltunnel <carpaltunnel-email>
2026-04-28 17:48:58 -0500

    Express

---
+++ 025-Express/code/body-parser.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello There!');
+});
+
+app.post('/posthere', (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
